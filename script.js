893c599b8e0c83dbdd917c6dc0d0adbcc38007bf
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2026-05-29 19:24:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+const output=document.getElementById("output");
+output.innerHTML="<h1>Hi My Name is Vamsi</h1>"
+
+let array=[1,2,3,4];
+let result=[];
+let Promise1=new Promise((resolve,reject)=>{
+	for(let i=0;i<array.length;i++){
+        if(array[i]%2==0){
+			result.push(array[i]);
+		}
+	} resolve(result)
+})
+Promise1.then((res)=>{
+	setTimeout(()=>{
+		output.innerText=`${result}`
+	},1000);
+	return result;
+	
+}).then((result)=>{
+	let mult=[];
+	for(let i=0;i<result.length;i++){
+		mult.push(result[i]*2);
+	}
+	setTimeout(()=>{
+		output.innerText=`${mult}`
+	},3000);
+})
+	.catch((e)=>{
+	output.innerHTML=`${e}`;
+})
+
+
+
+
+
+
